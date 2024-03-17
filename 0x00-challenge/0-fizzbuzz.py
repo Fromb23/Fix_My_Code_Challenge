d03e95da3fdc6ef991295f864a9bf6eade21bb4d
Fromb23 <rombo.programming@gmail.com>
2024-03-17 15:30:33 -0700

    fizzbuzz corrected

--- 0x00-challenge/0-fizzbuzz.py
+++ 0x00-challenge/0-fizzbuzz.py
@@ -21,11 +21,12 @@ def fizzbuzz(n):
             print("FizzBuzz", end=" ")
         elif (i % 3) == 0:
             print("Fizz", end=" ")
-        elif ( i % 5) == 0:
+        elif (i % 5) == 0:
             print("Buzz", end=" ")
         else:
             print(i, end=" ")
     print()
+
 
 if __name__ == '__main__':
     if len(sys.argv) <= 1:
